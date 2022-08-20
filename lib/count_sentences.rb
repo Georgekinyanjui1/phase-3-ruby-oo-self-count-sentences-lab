854032ce2aaf44317bfaef19c0464266b403f03b
georgekinyanjui1 <george.kinyanjui@student.moringaschool.com>
2022-08-20 12:28:49 +0300

    update file

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("").include?(".") ? true : false
   end
 
   def question?
-
+    self.split("").include?("?") ? true : false
   end
 
   def exclamation?
-
+    self.split("").include?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).length
   end
 end
+
